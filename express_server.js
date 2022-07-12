efc716f245656947352d9ac18d1e255bc131125b
Sszyh <zhuyer@outlook.com>
2022-07-12 09:29:58 -0700

    add hello

--- express_server.js
+++ express_server.js
@@ -13,7 +13,11 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
