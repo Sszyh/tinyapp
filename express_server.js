19488b1d374bc45a171ab63a4d445d64ab460de2
Sszyh <zhuyer@outlook.com>
2022-08-02 16:46:30 -0700

    add new render for urls/:id

--- express_server.js
+++ express_server.js
@@ -17,10 +17,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-})
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -35,6 +31,17 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase.id};//not sure for longURL
+  res.render("urls_show", templateVars);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
-})
+});
