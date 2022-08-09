b686b4b5a9af1672db5fe3cee6c95378a71cf2a7
Sszyh <zhuyer@outlook.com>
2022-08-09 16:46:28 -0700

    created urls_login template and sent a POST request to /login. Added get /login endpoint in server, updated _header template to display Register link and Login link.

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
-//tells the Express app to use EJS as its templating engine.
+/*tells the Express app to use EJS as its templating engine.*/
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -41,6 +41,7 @@ const findKeyByValue = function(object, value) {
   return output;
 };
 
+/*function to check if email already exist */
 const getUserByEmail = function(email) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -50,37 +51,19 @@ const getUserByEmail = function(email) {
   return false;
 }
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
 
 app.get("/urls", (req, res) => {//app.get is a route hanlder
   const templateVars = { 
     urls: urlDatabase,
     user: users[req.cookies["user_id"]]
   };
-  res.render("urls_index", templateVars);//use res.render() to pass the URL data to our template
-/*EJS automatically knows to look inside the views directory for any template files that have the extension .ejs. 
-This means we don't need to tell it where to find them. 
-It also means that we do not need to include the extension of the filename when referencing it.*/
+  console.log("cookies inside get urls:",[req.cookies["user_id"]]);
+  res.render("urls_index", templateVars);
+/*use res.render() to pass the URL data to our template.*/
 });
 
 app.get("/urls/new", (req, res) => {
@@ -101,6 +84,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   let shortId = generateRandomString();
   if (!Object.values(urlDatabase).includes(req.body.longURL)) {
@@ -112,15 +100,30 @@ app.post("/urls", (req, res) => {
   }
 });
 
+app.post("/urls/:id", (req, res) => {
+  let id = req.params.id;
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls`);
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect(`/urls`);
 });
 
-app.post("/urls/:id", (req, res) => {
-  let id = req.params.id;
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls`);
+app.get("/login", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_login", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_register", templateVars);
 });
 
 app.post("/login", (req, res) => {
@@ -130,29 +133,12 @@ app.post("/login", (req, res) => {
   res.redirect(`/urls`);
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect(`/urls`);
-});
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
-
-app.get("/register", (req, res) => {
-  const templateVars = { 
-    user: users[req.cookies["user_id"]]
-  };
-  res.render("register", templateVars);
-});
-
 app.post("/register", (req, res) => {
   let randomId = generateRandomString();
   let email = req.body.email;
   let password = req.body.password;
   res.cookie("user_id", randomId);
-
+  console.log("userbefore:", users);
   if (email.length === 0 || password.length === 0 ) {
     res.status(400).send("input can not be empty");
   } else if (getUserByEmail(email)) {
@@ -164,7 +150,12 @@ app.post("/register", (req, res) => {
       password: password
     };
     users[randomId] = userX;
-    console.log("userafter:", users)
+    console.log("userafter:", users);
     res.redirect(`/urls`);
   }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect(`/urls`);
 });
