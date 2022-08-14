57201564b4b301b876acfc89aef0d3810b34cd34
Sszyh <zhuyer@outlook.com>
2022-08-14 13:08:34 -0700

    add more test for helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers');
+const { getUserByEmail, findKeyByValue, findIdByEmail } = require('../helpers');
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
@@ -13,7 +13,7 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail',() => {
+describe('#getUserByEmail',() => {
   it('shoule return a true if email is valid', () => {
     const user = getUserByEmail('user@example.com', testUsers);
     assert.strictEqual(user, true);
@@ -21,5 +21,48 @@ describe('getUserByEmail',() => {
   it('should return false if emial is not in our users database', () => {
     const user = getUserByEmail('x@x',testUsers);
     assert.strictEqual(user, false);
-  })
-})
+  });
+});
+
+describe("#findKeyByValue", () => {
+  it("return 'drama' for 'The Wire'", () => {
+    const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+    };
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+
+  it("return 'undefined' for 'That '70s Show'", () => {
+    const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+    };
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+});
+
+describe('#findIdByEmail',() => {
+  it('shoule return a userID by email', () => {
+    const users = {
+      userRandomID: {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur",
+      }
+    };
+    assert.strictEqual(findIdByEmail("user@example.com", users), "userRandomID");
+  });
+  it('shoule return empty if email is not existed', () => {
+    const users = {
+      userRandomID: {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur",
+      }
+    };
+    assert.strictEqual(findIdByEmail("2@2", users), "");
+  });
+});
